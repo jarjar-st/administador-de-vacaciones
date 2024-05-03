819193edc86662dabc7c004a9a86ea06d871f000
Josue Meza <josue_benjamin12@hotmail.com>
2024-05-03 16:33:17 -0600

    user-schema

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/user.dto';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (user) {
+      throw new Error('Correo duplicado');
+    }
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...dto,
+        password: await hash(dto.password, 10),
+      },
+    });
+    const { password, ...result } = newUser;
+    return result;
+  }
+}
